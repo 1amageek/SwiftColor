e6cf8d2cc50da78f9e165d255d990b43e5328897
1amageek <nori@stamp.team>
2022-10-24 22:47:33 +0900

    Fix

--- Sources/SwiftColor/Color+Extension.swift
+++ Sources/SwiftColor/Color+Extension.swift
@@ -31,7 +31,7 @@ fileprivate extension Color {
         var g: CGFloat = 0
         var b: CGFloat = 0
         var a: CGFloat = 0
-
+        
         #if os(macOS)
         SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
         // Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
@@ -47,24 +47,85 @@ fileprivate extension Color {
     }
 }
 
-extension Color: Codable {
+fileprivate extension Color {
+    
+    /// https://developer.apple.com/design/human-interface-guidelines/foundations/color/#system-colors
+    static func from(name: String) -> Color? {
+        switch name {
+            case "red": return Color.red
+            case "orange": return Color.orange
+            case "yellow": return Color.yellow
+            case "green": return Color.green
+            case "mint": return Color.mint
+            case "teal": return Color.teal
+            case "cyan": return Color.cyan
+            case "blue": return Color.blue
+            case "indigo": return Color.indigo
+            case "purple": return Color.purple
+            case "pink": return Color.pink
+            case "brown": return Color.brown
+            case "white": return Color.white
+            case "gray": return Color.gray
+            case "black": return Color.black
+            case "clear": return Color.clear
+            case "primary": return Color.primary
+            case "secondary": return Color.secondary
+            default: return nil
+        }
+    }
+    
+    var name: String? {
+        switch self {
+            case Color.red: return "red"
+            case Color.orange: return "orange"
+            case Color.yellow: return "yellow"
+            case Color.green: return "green"
+            case Color.mint: return "mint"
+            case Color.teal: return "teal"
+            case Color.cyan: return "cyan"
+            case Color.blue: return "blue"
+            case Color.indigo: return "indigo"
+            case Color.purple: return "purple"
+            case Color.pink: return "pink"
+            case Color.brown: return "brown"
+            case Color.white: return "white"
+            case Color.gray: return "gray"
+            case Color.black: return "black"
+            case Color.clear: return "clear"
+            case Color.primary: return "primary"
+            case Color.secondary: return "secondary"
+            default: return nil
+        }
+    }
+}
 
+extension Color: Codable {
+    
     enum CodingKeys: String, CodingKey {
-        case red, green, blue, alpha
+        case name, red, green, blue, alpha
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let r = try container.decode(Double.self, forKey: .red)
-        let g = try container.decode(Double.self, forKey: .green)
-        let b = try container.decode(Double.self, forKey: .blue)
-        let a = try container.decode(Double.self, forKey: .alpha)
-        self.init(SystemColor(red: r, green: g, blue: b, alpha: a))
+        if let name = try container.decode(String?.self, forKey: .name) {
+            guard let color = Self.from(name: name) else {
+                throw EncodingError.invalidValue(name, EncodingError.Context(codingPath: [CodingKeys.name], debugDescription: "Invalid color name."))
+            }
+            self = color
+        } else {
+            let r = try container.decode(Double.self, forKey: .red)
+            let g = try container.decode(Double.self, forKey: .green)
+            let b = try container.decode(Double.self, forKey: .blue)
+            let a = try container.decode(Double.self, forKey: .alpha)
+            self.init(SystemColor(red: r, green: g, blue: b, alpha: a))
+        }
     }
 
     public func encode(to encoder: Encoder) throws {
         guard let colorComponents = self.colorComponents else { return }
+        let name = self.name
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
         try container.encode(colorComponents.red, forKey: .red)
         try container.encode(colorComponents.green, forKey: .green)
         try container.encode(colorComponents.blue, forKey: .blue)
